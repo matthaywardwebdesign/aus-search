556c0b6068ce0c54128e802ff83060bf4b29ab7a
Andrew Perry <me@andrewperry.com.au>
2017-07-04 23:07:21 +1000

    Update download.sh to mv data from right directory

--- download.sh
+++ download.sh
@@ -3,6 +3,6 @@ wget -O ./data/gnaf-latest.zip http://data.gov.au/dataset/19432f89-dc3a-4ef3-b94
 mkdir ./data/tmp
 unzip -o ./data/gnaf-latest.zip -d ./data/tmp
 rm -f ./data/*.psv
-mv ./data/tmp/FEB17\ GNAF\ PipeSeperatedValue/GNAF/G-NAF\ FEBRUARY\ 2017/Authority\ Code/*.psv ./data/
-mv ./data/tmp/FEB17\ GNAF\ PipeSeperatedValue/GNAF/G-NAF\ FEBRUARY\ 2017/Standard/*.psv ./data/
+mv ./data/tmp/MAY17_GNAF_PipeSeparatedValue_20170525135436/G-NAF/G-NAF\ MAY\ 2017/Authority\ Code/*.psv ./data/
+mv ./data/tmp/MAY17_GNAF_PipeSeparatedValue_20170525135436/G-NAF/G-NAF\ MAY\ 2017/Standard/*.psv ./data/
 rm -rf ./data/tmp
