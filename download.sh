012c8b06c713293cb307bbebd40256867d17a1fc
Andrew Perry <me@andrewperry.com.au>
2017-07-04 20:52:17 +1000

    Update download.sh to get the latest data
    Update the download.sh file to get the May 2017 data set.

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O ./data/gnaf-latest.zip http://data.gov.au/dataset/19432f89-dc3a-4ef3-b943-5326ef1dbecc/resource/99b44dff-4e84-4cb7-9cbf-a68d3ebf964a/download/feb17-gnaf-pipeseperatedvalue.zip
+wget -O ./data/gnaf-latest.zip http://data.gov.au/dataset/19432f89-dc3a-4ef3-b943-5326ef1dbecc/resource/99b44dff-4e84-4cb7-9cbf-a68d3ebf964a/download/may17gnafpipeseparatedvalue20170525135436.zip
 mkdir ./data/tmp
 unzip -o ./data/gnaf-latest.zip -d ./data/tmp
 rm -f ./data/*.psv
