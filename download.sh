7cfa429d66c32581bd295be982ef455025802904
matthaywardwebdesign <matt@matthayward.com.au>
2018-08-23 13:50:20 +1000

    Updated to use newest data

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget -O ./data/gnaf-latest.zip http://data.gov.au/dataset/19432f89-dc3a-4ef3-b943-5326ef1dbecc/resource/99b44dff-4e84-4cb7-9cbf-a68d3ebf964a/download/may17gnafpipeseparatedvalue20170525135436.zip
+wget -O ./data/gnaf-latest.zip https://data.gov.au/dataset/19432f89-dc3a-4ef3-b943-5326ef1dbecc/resource/4b084096-65e4-4c8e-abbe-5e54ff85f42f/download/may18gnafpipeseparatedvalue20180521161504.zip
 mkdir ./data/tmp
 unzip -o ./data/gnaf-latest.zip -d ./data/tmp
 rm -f ./data/*.psv
-mv ./data/tmp/MAY17_GNAF_PipeSeparatedValue_20170525135436/G-NAF/G-NAF\ MAY\ 2017/Authority\ Code/*.psv ./data/
-mv ./data/tmp/MAY17_GNAF_PipeSeparatedValue_20170525135436/G-NAF/G-NAF\ MAY\ 2017/Standard/*.psv ./data/
+mv ./data/tmp/MAY18_GNAF_PipeSeparatedValue_20180521161504/G-NAF/G-NAF\ MAY\ 2018/Authority\ Code/*.psv ./data/
+mv ./data/tmp/MAY18_GNAF_PipeSeparatedValue_20180521161504/G-NAF/G-NAF\ MAY\ 2018/Standard/*.psv ./data/
 rm -rf ./data/tmp
